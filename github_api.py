ea3988a2109c3930b723525913ae556c602c4272
Yousef Abuzahrieh <yousef_abuzahrieh@aol.com>
2023-07-17 15:09:50 -0400

    All data fetched. Cleaning time

--- github_api.py
+++ github_api.py
@@ -2,31 +2,32 @@ import asyncio
 import json
 
 from aiohttp import ClientSession
+from dataclasses import dataclass
+from datetime import datetime as dt
+from typing import NamedTuple
 from pathlib import Path
 from pprint import pprint
 
-def fetch_user():
-    git_user = json.load(open(Path.cwd() / 'git_user.json'))
-    username = git_user.get('username', 'Input username')
-    api_key = git_user.get('api_key', 'Input api_key')
-    return username, api_key
 
+class ConfigInfo(NamedTuple):
+    username: str
+    api_key: str
 
 
 class GitUserData:
-    def __init__(self):
-        self.username = fetch_user()[0]
-        self.api_key = fetch_user()[1]
+    def __init__(self, username, api_key):
+        self.username = username
+        self.api_key = api_key
         self.url = f'https://api.github.com/users/{self.username}'
     
-    async def parse_main_git(self, session):
+    async def parse_git(self, session):
         headers = {
             'Authorization': f'Bearer {self.api_key}',
             'User-Agent': 'MyGitHubDashboardProject/1.0'
         }
         async with session.get(self.url, headers=headers) as response:
             main_json = json.loads(json.dumps(await response.json()))
-
+            
             main_data = {
                         'name': main_json.get('name', None),
                         'user_avatar': main_json.get('avatar_url', None),
@@ -37,45 +38,66 @@ class GitUserData:
                         'public_gists': main_json.get('public_gists', 0),
                         'followers_count': main_json.get('followers', 0),
                         'following_count': main_json.get('following', 0),
-                        'account_created': main_json.get('created_at', None),
+                        'account_created': GitUserData.parse_date(main_json.get('created_at', None)),
                         'user_bio': main_json.get('bio', None),
                         'user_blog': main_json.get('blog', None)
                         }
 
             api_data = { 
-                    'main_github_page': main_json.get('html_url', None),
-                    'followers_url': main_json.get('followers_url', None),
-                    'following_url': main_json.get('following_url', None),
-                    'gists_url': main_json.get('gists_url', None),
-                    'repos_url': main_json.get('repos_url', None)
-                    }
+                        'main_github_page': main_json.get('html_url', None),
+                        'followers_url': main_json.get('followers_url', None),
+                        'following_url': main_json.get('following_url', None),
+                        'gists_url': main_json.get('gists_url', None),
+                        'repos_url': main_json.get('repos_url', None)
+                        }
             # Only fetching repo data for now
-            async def parse_repo_apis(repo_data, session):
-                async with session.get(repo_data.get('repos_url'), headers=headers) as response:
-                    try:
-                        repos_data = json.loads(json.dumps(await response.json()))
-                        # repos_json = open(Path.cwd() / 'repos_data.json', 'w', encoding='utf-8')
-                        pprint(repos_data, sort_dicts=False)
-                    except:pass
+            async def parse_url_apis(api_data, session):
+                url_info = {}
+                async with session as session:
+                    for key, url in api_data.items():
+                        if url:
+                            async with session.get(url, headers=headers) as response:
+                                try:
+                                    data = json.loads(json.dumps(await response.json()))
+                                    url_info[key] = data
+                                except:
+                                    pass
+                return url_info
 
-            await parse_repo_apis(api_data, session)
+            api_info = await parse_url_apis(api_data, session)
+        return main_data, api_info
+    
+    @staticmethod
+    def load_json(file):
+        return json.load(open(Path(__file__).parent.absolute() / file))
+    
+    @staticmethod
+    def parse_date(date_):
+        if date_ is None:
+            return None
         
-        
+        date_ = ' '.join(date_.split('T'))[:-1]
+        return dt.strptime(date_, '%Y-%m-%d %H:%M:%S').strftime('%I:%M:%S%p %m-%d-%Y')
+    
+    def clean_data(self):
+        pass
         
         # ** Create separate modules to handle data processing, analytics, and visualization.
         # TODO for GitHub API Integration:
             # ? Implement methods in this class to fetch:
             # ? repository details, commit history, pull requests, and other relevant information.
             # ? Once data is all fetched and organized, follow up with data processing and analytics
-            
+            # ? Make a large sql database containing all information on each repo
             
 
 async def main():
+    config_file = GitUserData.load_json('config.json')
+    config = ConfigInfo(*config_file.values())
+    
     async with ClientSession() as session:
-        git_user_data = GitUserData()
-        await asyncio.gather(
-            git_user_data.parse_main_git(session)
-        )
+        git_user_data = GitUserData(*config)
+        main_data, repos_data = await git_user_data.parse_git(session)
+        pprint(repos_data, sort_dicts=False)
 
 if __name__ == '__main__':
     asyncio.run(main())
